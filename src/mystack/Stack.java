34bd8d886b291504005b8a4a6aef804e5f0fc36c
USER <USER@DESKTOP-R9AKR1U>
2019-09-15 11:19:12 +0530

    ex06

---
+++ src/mystack/Stack.java
@@ -0,0 +1,15 @@
+/**created by m.uday kanth, 
+ * eee-b, 212217105037
+ * 
+ */
+
+package mystack;
+
+public interface Stack {
+	public void push(String v)  throws StackException;
+	public String pop() throws StackException;
+	
+
+}
+
+
---
+++ src/mystack/StackException.java
@@ -0,0 +1,13 @@
+/**created by m.uday kanth, 
+ * eee-b, 212217105037
+ * 
+ */
+package mystack;
+
+public class StackException extends Exception {
+	public StackException(String m)
+	{
+		super(m);
+	}
+
+}
