da1e83cef42fa8592a64c16efb34360bce90fc31
user3 <user3@simlab16>
2019-09-16 15:32:31 +0530

    To design a java interface for ADK stack using string data.

---
+++ src/mystack/Stack.java
@@ -0,0 +1,17 @@
+/*
+ * developed by R.Nehareddy
+ * EEE-B
+ * 212217105049
+ * 
+ * 
+ */
+
+package mystack;
+
+public interface Stack {
+	public void push(String v)  throws StackException;
+	public String pop() throws StackException;
+	
+
+}
+
---
+++ src/mystack/StackException.java
@@ -0,0 +1,17 @@
+/*
+ * developed by R.Nehareddy
+ * EEE-B
+ * 212217105049
+ * 
+ * 
+ */
+
+package mystack;
+
+public class StackException extends Exception {
+	public StackException(String m)
+	{
+		super(m);
+	}
+
+}
