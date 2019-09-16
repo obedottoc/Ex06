21543f421fd8648940aaf7eafbb370ec9a079cdf
user3 <user3@simlab26>
2019-09-16 19:51:23 +0530

    modified for experiment 06

---
+++ src/mystack/Mystack.java
@@ -0,0 +1,40 @@
+package mystack;
+
+public class Mystack implements Stack {
+	private String data[];
+	private int top;
+
+	public Mystack(int s) 
+	{
+		top=-1;
+		data=new String[s];
+	}
+	
+	@Override
+	public void push(String v) throws StackException
+	{
+	
+		if(top>=(data.length-1))
+		{
+			throw new StackException("Stack Full: It is already having "+(top+1)+" elements");
+		}
+		top=top+1;
+		data[top]=v;
+
+	}
+
+	@Override
+	public String pop()throws StackException 
+	{
+		String result;
+		if(top<0)
+		{
+			throw new StackException("Stack is empty");
+		}
+		result=data[top];
+		top=top-1;
+		
+		return result;
+	}
+
+}
