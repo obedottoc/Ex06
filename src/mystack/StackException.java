020c07d235d6ecc5dd46c86f739f2ce0b6297949
user2 <user2@administrator-HP-Pro-3330-MT>
2019-09-19 12:13:23 +0530

    created by keshaw shree

---
+++ src/mystack/StackException.java
@@ -0,0 +1,14 @@
+package mystack;
+
+@SuppressWarnings("serial")
+public class StackException extends Exception {
+   
+    public StackException(String m)
+    {
+        super(m);
+    }
+}
+
+
+
+
