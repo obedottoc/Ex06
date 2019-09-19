2e07080ee9f8a5e89e5fdf0d0546f672c208f813
user2 <user2@administrator-HP-Pro-3330-MT>
2019-09-19 11:35:14 +0530

    exp 6

---
+++ src/mystack/Stack.java
@@ -0,0 +1,12 @@
+/***
+ *lokesh
+ * lokeshwarn2000@gmail.com
+ * exp 6
+ */
+package mystack;
+
+public interface Stack {   
+    public void push(String v)  throws StackException;
+    public String pop() throws StackException;
+}
+
---
+++ src/mystack/StackException.java
@@ -0,0 +1,15 @@
+/***
+ *lokesh
+ * lokeshwarn2000@gmail.com
+ * exp 6
+ */
+package mystack;
+
+public class StackException extends Exception {
+   
+    public StackException(String m)
+    {
+        super(m);
+    }
+}
+
