a45664ff41697c9c384eebf26c23fdedd120dc6c
user2 <user2@administrator-HP-Pro-3330-MT>
2019-09-26 09:25:44 +0530

    kaushik

---
+++ src/Fileinfo/Stack.java
@@ -0,0 +1,11 @@
+/**
+ * devloped by kaushik kumar
+ * eee-a
+ */
+
+package Fileinfo;
+
+public interface Stack {   
+    public void push(String v)  throws StackException;
+    public String pop() throws StackException;
+}
---
+++ src/Fileinfo/StackException.java
@@ -0,0 +1,16 @@
+/**
+ * devloped by kaushik kumar
+ * eee-a
+ */
+
+package Fileinfo;
+
+public class StackException extends Exception {
+   
+    public StackException(String m)
+    {
+        super(m);
+    }
+}
+
+
